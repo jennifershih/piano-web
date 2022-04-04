641ca56be740ecd2253d6132c223271cc8e07e18
jennifershih <s01059810@gmail.com>
2022-04-05 00:04:18 +0800

    Add unmute button

--- src/views/PianoView.vue
+++ src/views/PianoView.vue
@@ -1,15 +1,22 @@
 <template>
   <h1>This is a Piano page</h1>
-
+  <button @click="unmute">Unmute</button>
   <Keyboard />
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
+import { start } from "tone";
 import Keyboard from "@/components/Keyboard.vue";
 
-export default {
+export default defineComponent({
   components: { Keyboard },
-};
+  methods: {
+    async unmute() {
+      await start();
+    },
+  },
+});
 </script>
 
 <style></style>
