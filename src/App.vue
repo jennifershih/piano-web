30084b690b8d65b9f7a95cbaa13db7fb35efacef
jennifershih <s01059810@gmail.com>
2022-04-07 22:59:05 +0800

    Fix bugs after deploy to gh-page
    - add base url when build for production
    - using router-link instead of a#href

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
-<script setup lang="ts"></script>
-
 <template>
   <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
-      <a class="nav-link active navbar-brand mb-0 h1" href="/">
-        <img src="@/assets/piano-icon.png" width="30" height="24" />
-        Home</a
-      >
-      <a class="nav-link navbar-brand mb-0 h1" href="/piano">
-        <img src="@/assets/piano-icon.png" width="30" height="24" />
-        Piano</a
-      >
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <RouterLink to="/" class="navbar-brand">
+          <img
+            src="@/assets/piano-icon.png"
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          />
+          Piano Web
+        </RouterLink>
+      </div>
     </nav>
   </header>
   <main>
@@ -18,6 +19,15 @@
   </main>
   <footer class="footer-copyright text-center">© 2022 Jennifer Shih</footer>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+
+export default defineComponent({
+  components: { RouterView, RouterLink },
+});
+</script>
 
 <style lang="scss">
 @import "@/assets/styles.scss";
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container-fluid text-center">
-    <h1>線上鋼琴</h1>
+    <h1>Piano Web</h1>
     <p>使用 Vue.js、Bootstrap 做出的線上鋼琴。</p>
     <p>用滑鼠來彈奏鋼琴，每個琴鍵會對應到不同的音調。</p>
-    <a href="/piano" class="btn btn-lg btn-success">開始彈奏</a>
+    <RouterLink to="/piano" class="btn btn-lg btn-success">開始彈奏</RouterLink>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { RouterLink } from "vue-router";
 
-export default defineComponent({});
+export default defineComponent({
+  components: { RouterLink },
+});
 </script>
